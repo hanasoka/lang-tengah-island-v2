3f11887a53d0aedeb8995b7c028de833f3d3049a
hanasoka <57451015+hanasoka@users.noreply.github.com>
2023-12-14 03:16:26 +0000

    added api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
 
@@ -36,6 +36,10 @@ TOURS = [
 @app.route("/")
 def hello_world():
     return render_template("home.html", tours=TOURS, name="Lang Tengah Island")
+
+@app.route("/api/tours")
+def list_tour():
+    return jsonify(TOURS)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)  
