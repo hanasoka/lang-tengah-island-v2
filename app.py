836573aa191f3f868f33516f4639c7c5019228e6
hanasoka <57451015+hanasoka@users.noreply.github.com>
2024-01-16 15:09:23 +0000

    changes some python codes

--- app.py
+++ app.py
@@ -1,48 +1,15 @@
 from flask import Flask, render_template, jsonify 
-from database import engine
+from database import load_tours_from_db
+
 
 app = Flask(__name__)
 
-TOURS = [
-  {
-    "id": 1,
-    "title": "Day Trip",
-    "description": "A day trip to Lang Tengah Island",
-    "price": "MYR 500.00"
-  },
-
-  {
-    "id": 2,
-    "title": "Family Picnic",
-    "description": "A beach picnic at Turtle Bay",
-    "price": "MYR 1,000.00"
-  },
-
-  {
-    "id": 3,
-    "title": "Group Trip",
-    "description": "Explore Lang Tengah Island",
-    "price": "MYR 5,000.00"
-  },
-
-  {
-    "id": 4,
-    "title": "Private Trip",
-    "description": "Picnic, Adventure, Exploration",
-
-  }
-]
-
-def load_tours_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from tours"))
-    result_dicts = []
-    for row in result.all():
-        result_dicts.append(row._asdict())
-
 @app.route("/")
 def hello_world():
-    return render_template("home.html", tours=TOURS, name="Lang Tengah Island")
+    tours_list = load_tours_from_db()
+    return render_template("home.html", 
+                           tours= tours_list, 
+                           name="Lang Tengah Island")
 
 @app.route("/api/tours")
 def list_tour():
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text 
+from sqlalchemy import create_engine, text
 
 # Becareful with username & password before commit to github, planetscale may revoke it.
 db_connection_string = 
@@ -10,5 +10,12 @@ engine = create_engine(db_connection_string, connect_args={
   }
 })
 
+def load_tours_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from tours"))
+    tours = []
+    for row in result.all():
+        tours.append(row._asdict())
+    return tours
 
 
