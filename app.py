15be9e6e47de1c11275c5bb5e9c30454b9e09606
hanasoka <57451015+hanasoka@users.noreply.github.com>
2024-01-23 08:20:35 +0000

    make changes to codes

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
-from flask import Flask, render_template, jsonify 
+from flask import Flask, render_template, jsonify, request
 from database import load_tours_from_db, load_tour_from_db
-
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    tours_list = load_tours_from_db()
-    return render_template("home.html", 
-                           tours= tours_list, 
-                           name="Lang Tengah Island")
+  tours_list = load_tours_from_db()
+  return render_template("home.html",
+                         tours=tours_list,
+                         name="Lang Tengah Island")
+
 
 @app.route("/api/tours")
 def list_tour():
-    tours_list = load_tours_from_db()
-    return jsonify(tours_list)
+  tours_list = load_tours_from_db()
+  return jsonify(tours_list)
+
 
 @app.route("/tour/<id>")
 def show_tour(id):
-    tour = load_tour_from_db(id)
-    if not tour:
-        return "Not Found", 404
-    return render_template("tourpage.html", 
-                           tour=tour, 
-                           name="Experience Lang Tengah")
+  tour = load_tour_from_db(id)
+  if not tour:
+    return "Not Found", 404
+  return render_template("tourpage.html",
+                         tour=tour,
+                         name="Experience Lang Tengah")
+
+
+@app.route("/tour/<id>/book", methods=["post"])
+def book_tour(id):
+  data = request.form
+  tour = load_tour_from_db(id)
+  # store this is DB
+  # send an email
+  # display an acknowledgement
+  return render_template("booking_submitted.html", booking=data, name=tour)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)  
+  app.run(host="0.0.0.0", debug=True)
